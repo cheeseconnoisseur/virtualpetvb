13993d6d6459aac96846fdc17ea5a0698c4004fb
cheeseconnoisseur <arturo.dezon@gmail.com>
2017-12-17 19:38:18 +0000

    initial commit

---
+++ virtual pet game/Form1.Designer.vb
@@ -0,0 +1,258 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ftank = New System.Windows.Forms.TextBox()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.mony = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.happyy = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.hung = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cleany = New System.Windows.Forms.Label()
+        Me.label = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Timer5 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'ftank
+        '
+        Me.ftank.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.ftank.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.ftank.Location = New System.Drawing.Point(325, 223)
+        Me.ftank.Multiline = True
+        Me.ftank.Name = "ftank"
+        Me.ftank.Size = New System.Drawing.Size(699, 363)
+        Me.ftank.TabIndex = 0
+        Me.ftank.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'B1
+        '
+        Me.B1.Location = New System.Drawing.Point(31, 28)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(174, 39)
+        Me.B1.TabIndex = 1
+        Me.B1.Text = "start/restart"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(325, 95)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(698, 134)
+        Me.TextBox1.TabIndex = 2
+        '
+        'mony
+        '
+        Me.mony.AutoSize = True
+        Me.mony.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.mony.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.mony.Location = New System.Drawing.Point(835, 28)
+        Me.mony.Name = "mony"
+        Me.mony.Size = New System.Drawing.Size(145, 48)
+        Me.mony.TabIndex = 3
+        Me.mony.Text = "Label1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(868, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "money"
+        '
+        'happyy
+        '
+        Me.happyy.AutoSize = True
+        Me.happyy.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.happyy.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.happyy.Location = New System.Drawing.Point(666, 28)
+        Me.happyy.Name = "happyy"
+        Me.happyy.Size = New System.Drawing.Size(145, 48)
+        Me.happyy.TabIndex = 5
+        Me.happyy.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(706, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "happiness"
+        '
+        'hung
+        '
+        Me.hung.AutoSize = True
+        Me.hung.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.hung.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.hung.Location = New System.Drawing.Point(495, 28)
+        Me.hung.Name = "hung"
+        Me.hung.Size = New System.Drawing.Size(145, 48)
+        Me.hung.TabIndex = 7
+        Me.hung.Text = "Label1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(531, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "hunger"
+        '
+        'cleany
+        '
+        Me.cleany.AutoSize = True
+        Me.cleany.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.cleany.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!)
+        Me.cleany.Location = New System.Drawing.Point(317, 28)
+        Me.cleany.Name = "cleany"
+        Me.cleany.Size = New System.Drawing.Size(145, 48)
+        Me.cleany.TabIndex = 9
+        Me.cleany.Text = "Label1"
+        '
+        'label
+        '
+        Me.label.AutoSize = True
+        Me.label.Location = New System.Drawing.Point(349, 6)
+        Me.label.Name = "label"
+        Me.label.Size = New System.Drawing.Size(82, 17)
+        Me.label.TabIndex = 10
+        Me.label.Text = "cleanisness"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(40, 129)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(231, 88)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "feed " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "-15 money" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "+5 happiness" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "- 20 hunger" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(39, 232)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(227, 81)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "clean tank" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "-5 money" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "+ 5 happiness" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "+35 cleaniness"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(40, 331)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(226, 81)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "play with fish " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "-5 money" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "+25 hapiness"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        '
+        'Timer3
+        '
+        '
+        'Timer4
+        '
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(42, 428)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(223, 78)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "upgrade your fish" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "-30 money"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Timer5
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(1032, 592)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.label)
+        Me.Controls.Add(Me.cleany)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.hung)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.happyy)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.mony)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.ftank)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "fish simulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ftank As TextBox
+    Friend WithEvents B1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents mony As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents happyy As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents hung As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cleany As Label
+    Friend WithEvents label As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Timer5 As Timer
+End Class
---
+++ virtual pet game/Form1.vb
@@ -0,0 +1,378 @@
+﻿Public Class Form1
+    Dim mon As Integer = 10000
+    Dim happy As Integer = 50
+    Dim clean As Integer = 100
+    Dim hunger As Integer = 0
+    Dim msg = "game over"
+    Dim age As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 10000 '10 seconds
+        Timer1.Enabled = True
+
+        Timer2.Interval = 21000 '20 seconds
+        Timer2.Enabled = True
+
+        Timer3.Interval = 25000 '20 seconds
+        Timer3.Enabled = True
+
+        Timer4.Interval = 30000 '20 seconds
+        Timer4.Enabled = True
+
+        Timer5.Interval = 1000 '20 seconds
+        Timer5.Enabled = True
+
+
+        If age = 1 Then
+            ftank.Text = "
+
+
+
+
+
+
+
+
+  ,
+ <><
+  `
+"
+        ElseIf age = 2 Then
+            ftank.Text = "
+
+
+
+
+
+  /
+ /\/
+ \/\
+  \  "
+
+        ElseIf age = 3 Then
+            ftank.Text = "
+
+
+
+
+
+  /
+,'`./
+`.,'\
+  \  "
+        ElseIf age = 4 Then
+            ftank.Text = "
+
+
+
+
+
+   ,
+  /|
+ /_/ ,
+/o \/|
+\<_/\|
+ \ \ `
+  \| 
+   `"
+        ElseIf age = 5 Then
+            ftank.Text = "
+
+
+
+
+
+   ,
+  /|
+ /_|  ,
+/o  \/|
+\<__/\|
+ \ |  `
+  \|
+   `"
+        End If
+
+
+    End Sub
+
+    Private Sub B1_Click(sender As Object, e As EventArgs) Handles B1.Click
+
+        age = 1
+        ftank.Text = "
+
+
+
+
+
+        ,
+       ()<  
+        `"
+        mon = 10000
+        mony.Text = (mon & "/100")
+        happy = 50
+        happyy.Text = (happy & "/100")
+        hunger = 99
+        hung.Text = (hunger & "/100")
+        clean = 100
+        cleany.Text = (clean & "/100")
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        mon = mon + 1
+        mony.Text = (mon & "/100")
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        hunger = hunger + 1
+        hung.Text = (hunger & "/100")
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        happy = happy - 1
+        happyy.Text = (happy & "/100")
+
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        clean = clean - 1
+        cleany.Text = (clean & "/100")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        mon = mon - 15
+        mony.Text = (mon & "/100")
+        happy = happy + 5
+        happyy.Text = (happy & "/100")
+        hunger = hunger - 20
+        hung.Text = (hunger & "/100")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        mon = mon - 5
+        mony.Text = (mon & "/100")
+        happy = happy + 5
+        happyy.Text = (happy & "/100")
+        clean = clean + 35
+        cleany.Text = (clean & "/100")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        mon = mon - 5
+        mony.Text = (mon & "/100")
+        happy = happy + 25
+        happyy.Text = (happy & "/100")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If age = 1 Then
+            mon = mon - 30
+            mony.Text = (mon & "/100")
+            ftank.Text = "
+
+
+
+
+
+  /
+ /\/
+ \/\
+  \  "
+            age = 2
+
+        ElseIf age = 2 Then
+            mon = mon - 30
+            mony.Text = (mon & "/100")
+            ftank.Text = "
+
+
+
+
+
+  /
+,'`./
+`.,'\
+  \  "
+            age = 3
+        ElseIf age = 3 Then
+            mon = mon - 30
+            mony.Text = (mon & "/100")
+            ftank.Text = "
+
+
+
+
+
+   ,
+  /|
+ /_/ ,
+/o \/|
+\<_/\|
+ \ \ `
+  \| 
+   `"
+            age = 4
+
+        ElseIf age = 4 Then
+            mon = mon - 30
+            mony.Text = (mon & "/100")
+            ftank.Text = "
+
+
+
+
+
+   ,
+  /|
+ /_|  ,
+/o  \/|
+\<__/\|
+ \ |  `
+  \|
+   `"
+            age = 5
+        ElseIf age = 5 Then
+            mon = mon - 30
+            mony.Text = (mon & "/100")
+            ftank.Text = "
+
+
+
+
+
+
+       /`-._
+      /_,.._`:-         
+  ,.-'  ,   `-:..-')   
+ : o ):';      _  {   
+  `-._ `'__,.-'\`-.)
+      `\\  \,.-'`"
+
+            age = 6
+
+        ElseIf age = 6 Then
+            mon = mon - 30
+            mony.Text = (mon & "/100")
+            ftank.Text = "
+
+
+
+        o                 o
+                  o
+         o   ______      o
+           _/  (   \_
+ _       _/  (       \_  O
+| \_   _/  (   (    0  \
+|== \_/  (   (          |
+|=== _ (   (   (        |
+|==_/ \_ (   (          |
+|_/     \_ (   (    \__/
+          \_ (      _/
+            |  |___/
+           /__/"
+            age = 7
+        ElseIf age = 7 Then
+            mon = mon - 30
+            mony.Text = (mon & "/100")
+            ftank.Text = "
+
+
+               __   __
+              __ \ / __
+             /  \ | /  \
+                 \|/
+            _,.---v---._
+   /\__/\  /            \
+   \_  _/ /              \ 
+     \ \_|           @ __|
+      \                \_
+       \     ,__/       /"
+            age = 8
+        End If
+    End Sub
+
+    Private Sub Timer5_Tick(sender As Object, e As EventArgs) Handles Timer5.Tick
+
+        If happy < 0 Then
+            ftank.Text = "
+
+
+
+|\    \ \ \ \ \ \ \      __   
+|  \    \ \ \ \ \ \ \   | O~-_
+|   >----|-|-|-|-|-|-|--|  __/
+|  /    / / / / / / /   |__\  
+|/     / / / / / / /
+
+"
+
+            MsgBox(msg)
+
+        ElseIf happy < 15 Then
+            happyy.BackColor = Color.Red
+
+        ElseIf happy < 35 Then
+            happyy.BackColor = Color.Yellow
+
+
+        End If
+
+        If mon < 0 Then
+            ftank.Text = "
+
+
+
+|\    \ \ \ \ \ \ \      __   
+|  \    \ \ \ \ \ \ \   | O~-_
+|   >----|-|-|-|-|-|-|--|  __/
+|  /    / / / / / / /   |__\  
+|/     / / / / / / /
+
+"
+
+            MsgBox(msg)
+
+        ElseIf mon < 20 Then
+            mony.BackColor = Color.Red
+        ElseIf mon < 35 Then
+            mony.BackColor = Color.Yellow
+
+        End If
+
+
+        If hunger > 100 Then
+            ftank.Text = "
+
+
+
+|\    \ \ \ \ \ \ \      __   
+|  \    \ \ \ \ \ \ \   | O~-_
+|   >----|-|-|-|-|-|-|--|  __/
+|  /    / / / / / / /   |__\  
+|/     / / / / / / /
+
+"
+
+            MsgBox(msg)
+
+        ElseIf hunger > 90 Then
+            hung.BackColor = Color.Red
+        ElseIf hunger > 70 Then
+            hung.BackColor = Color.Yellow
+
+        End If
+        If clean < 0 Then
+            happy = happy - 20
+
+        ElseIf clean < 20 Then
+            mony.BackColor = Color.Red
+        ElseIf clean < 35 Then
+            mony.BackColor = Color.Yellow
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
+
